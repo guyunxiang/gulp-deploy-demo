6285e9cac03eea579f7198bdd8db906cd85806fd
顾云翔 <guyunxiang@renrenfenqi.com>
2016-03-30 10:11:35 +0800

    add gulp-sass

---
+++ gulp-sass/gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp'),
+  sass = require('gulp-sass');
+
+// 编译scss文件
+gulp.task('sass', function() {
+  gulp.src('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('css'))
+});
+
+// 启动监听自动编译
+gulp.task('watch', function() {
+  gulp.watch('sass/**/*.scss', ['sass'])
+});
